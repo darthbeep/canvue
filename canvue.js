4772d1f1b74f0c74f66b1b2e1a307a004039bb3d
darthbeep <darthbeep@gmail.com>
2018-02-09 00:46:42 -0500

    did the dvd thing

--- canvue.js
+++ canvue.js
@@ -1,5 +1,5 @@
 //Setup
-var mode = "anim", size = 10, sizedir = true;
+var mode = "anim", size = 10, sizedir = true, x=150, y=150, xdir = true, ydir = true;
 var bnames = ['', 'I\'m an Animaniac', 'STOP', 'I’m waiting for the movie to start'];
 var buttonlist = ['', 'anim', 'stop', 'dvd'];
 //Vue stuff
@@ -25,18 +25,20 @@ var app = new Vue({
 var c=document.getElementById('canvas');
 var ctx=c.getContext("2d");
 var anim = function() {
-    mode="anim"
+    var x = 150;
+    var y = 150;
+    mode="anim";
 }
 var stop = function() {
     mode="stop"
 }
 var dvd = function() {
-    mode="dvd"
+    size=10;
+    mode="dvd";
 }
 var doStuff = function() {
     if (mode=="anim") {
         ctx.clearRect(0, 0, c.width, c.height);
-        console.log(sizedir);
         sizedir ? size++ : size--;
         if (size > 20 || size < 1) {
             sizedir = !sizedir;
@@ -47,7 +49,22 @@ var doStuff = function() {
         ctx.fill();
     }
     if (mode == "dvd") {
-
+        ctx.clearRect(0, 0, c.width, c.height);
+        //I feel like I got really fancy here, but essentially I'm just controlling the directions
+        //With ternary statments though
+        //That's pretty awesome
+        xdir ? x+=2 : x-=2;
+        ydir ? y++ : y--;
+        if (x > 300 || x < 1) {
+            xdir = !xdir;
+        }
+        if (y > 300 || y < 1) {
+            ydir = !ydir;
+        }
+        ctx.beginPath();
+        ctx.arc(x, y, size, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.fill();
     }
     if (mode == "stop") {
     }
